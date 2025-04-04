a9afb642737ef7e67a21a87b18d9dfd2f83dcd5d
Maïssane Abdallah <abd.maissane@gmail.com>
2025-04-04 02:44:18 +0200

    ormulaire pour ajouter un livre (title et author).

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 import { Book } from "./book.js";
 import { Library } from "./library.js"
 
-const Cyrano = new Book ("Cyrano de Bergerac", "Edmond Rostand")
-const Malade = new Book ("Le Malade imaginaire", "Moliere")
-const librairie = new Library();
-console.log(Cyrano.getDetails());
-librairie.addBook(Cyrano);
-librairie.addBook(Malade);
-librairie.listBooks();
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+//   console.log(form.elements.title.value);
+//   console.log(event.target.autheur.value);
+  const newBook = new Book (form.elements.title.value, event.target.autheur.value)
+  const librairie = new Library();
+  librairie.addBook(newBook);
+  librairie.listBooks();
 
-console.log(librairie.findBookByTitle("Cyrano de Bergerac")); 
-console.log(librairie.findBookByTitle("L'Étranger")); 
+});
+
+// const Cyrano = new Book ("Cyrano de Bergerac", "Edmond Rostand")
+// const Malade = new Book ("Le Malade imaginaire", "Moliere")
+// const librairie = new Library();
+// console.log(Cyrano.getDetails());
+// librairie.addBook(Cyrano);
+// librairie.addBook(Malade);
+// librairie.listBooks();
+
+// console.log(librairie.findBookByTitle("Cyrano de Bergerac")); 
+// console.log(librairie.findBookByTitle("L'Étranger")); 
